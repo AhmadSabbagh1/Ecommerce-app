f7041477fb64991bf39c970ccc84b079696bc79b
AhmadSabbagh1 <109554944+AhmadSabbagh1@users.noreply.github.com>
2024-09-09 15:34:03 +0300

    Creating pages and routing and adding routing to the App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Content'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
-    <div>
-      
+
+    <div className='px-4 sm:px-[5vw] m:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/collection' element={<Collection/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/product/:productId' element={<Product/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/place-order' element={<PlaceOrder/>}></Route>
+        <Route path='/orders' element={<Orders/>}></Route>
+
+      </Routes>
+
     </div>
   )
 }
